33e86fcf4d484bc7067de73492f8418e8ed9d240
RAJEEV SINHA <rajeev.sinha@gmail.com>
2020-09-27 03:07:55 -0500

    This is my first commit

---
+++ AppXtreme/AppXtreme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppXtreme
+//
+//  Created by RAJEEV SINHA on 9/27/20.
+//  Copyright © 2020 AppXtreme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AppXtreme/AppXtremeTests/AppXtremeTests.swift
@@ -0,0 +1,34 @@
+//
+//  AppXtremeTests.swift
+//  AppXtremeTests
+//
+//  Created by RAJEEV SINHA on 9/27/20.
+//  Copyright © 2020 AppXtreme. All rights reserved.
+//
+
+import XCTest
+@testable import AppXtreme
+
+class AppXtremeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
